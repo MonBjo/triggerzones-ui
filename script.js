8edac69d62fb045fda1ca7000f12dfc55c76d27a
Monica Björk <m.bjork95@gmail.com>
2024-05-23 01:08:35 +0200

    all eventlisteners added, get color function added

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // REMEMBER TO REMOVE THE BACKGROUND
 
 let tableElem = document.getElementById("table");
+
 let mainFormElem = document.getElementById("mainForm");
+let altitudeElem = document.getElementById("altitude");
+let heightElem = document.getElementById("height");
+let cancleButtonElem = document.getElementById("cancle");
+
 let colorFormElem = document.getElementById("colorForm");
+let activeColorElems = document.getElementsByClassName("activeZoneColor");
+let inactiveColorElems = document.getElementsByClassName("inactiveZoneColor");
+let activeRGBAA = getRGBAA(activeColorElems);
+let inactiveRGBAA = getRGBAA(inactiveColorElems);
 
 let alertElem = document.getElementById("alert");
 let alertMessageElem = document.getElementById("alertMessage");
@@ -11,22 +20,60 @@ let alertButtonElem = document.getElementById("alertButton");
 // debug info
 console.log(tableElem);
 console.log(mainFormElem);
-console.log(colorFormElem);
-console.log(alertElem);
-console.log(alertMessageElem);
-console.log(alertButtonElem);
 
-// more debug info
-showElem(alertElem);
-// setTimeout(() => {
-//     showModalMessage(alertElem, alertMessageElem, "this is a message for the alert");
-//     console.log("Delayed for 1 second.");
-// }, "1000");
+// console.log(colorFormElem);
+// console.log(activeColorElems);
+// console.log(inactiveColorElems);
 
+// console.log(alertElem);
+// console.log(alertMessageElem);
+// console.log(alertButtonElem);
+
+mainFormElem.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("form was submitted");
+    console.log(event);
+});
+
+altitudeElem.addEventListener("change", (event) => {
+    console.log(altitudeElem.value);
+});
+
+heightElem.addEventListener("change", (event) => {
+    console.log(heightElem.value);
+});
 
 alertButtonElem.addEventListener("click", (event) => {
     hideElem(alertElem);
 });
+
+cancleButtonElem.addEventListener("click", (event) => {
+    console.log("byebye");
+});
+
+
+function getRGBAA(RGBAAinput) {
+    let RGBAAvalues = {
+        color: [],
+        lines: 0,
+        walls: 0
+    }
+
+    for(let i = 0; i < RGBAAinput.length; i++){
+        if (i < 3) {
+            RGBAAvalues.color.push(RGBAAinput[i].value);
+        } else if (i == 3) {
+            RGBAAvalues.lines = RGBAAinput[i].value;
+        } else if (i == 4) {
+            RGBAAvalues.walls = RGBAAinput[i].value;
+        } else {
+            console.log(i);
+            console.log("Something odd is happening in the [getRGBAA] function");
+        }
+    }
+
+    return RGBAAvalues;
+}
 
 
 function hideElem(elem) {
