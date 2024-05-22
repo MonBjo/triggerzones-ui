22bbe02290be4dfbf601920fa65de547be25d973
Monica Björk <m.bjork95@gmail.com>
2024-05-22 22:42:23 +0200

    added function to the modal/alert

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 // REMEMBER TO REMOVE THE BACKGROUND
 
-let alertElem = document.getElementById("alert");
 let tableElem = document.getElementById("table");
 let mainFormElem = document.getElementById("mainForm");
 let colorFormElem = document.getElementById("colorForm");
 
-console.log(alertElem);
+let alertElem = document.getElementById("alert");
+let alertMessageElem = document.getElementById("alertMessage");
+let alertButtonElem = document.getElementById("alertButton");
+
+// debug info
 console.log(tableElem);
 console.log(mainFormElem);
 console.log(colorFormElem);
+console.log(alertElem);
+console.log(alertMessageElem);
+console.log(alertButtonElem);
 
-// showElem(alertElem);
+// more debug info
+showElem(alertElem);
+// setTimeout(() => {
+//     showModalMessage(alertElem, alertMessageElem, "this is a message for the alert");
+//     console.log("Delayed for 1 second.");
+// }, "1000");
+
+
+alertButtonElem.addEventListener("click", (event) => {
+    hideElem(alertElem);
+});
+
 
 function hideElem(elem) {
     elem.className = "hidden";
     console.log(`Element [${elem}] is now hidden.`);
 }
+
 function showElem(elem) {
     let classes = elem.classList;
     
@@ -27,4 +45,9 @@ function showElem(elem) {
             console.log(`Element [${elem.id}] is already visible`);
         }
     }
+}
+
+function showModalMessage(modalElem, messageElem, message) {
+    messageElem.innerHTML = `<p>${message}</p>`;
+    showElem(modalElem);
 }
