e1fc6051b8aa1043a5fe5222c437cb41b05b590a
Monica Björk <m.bjork95@gmail.com>
2024-05-28 23:34:46 +0200

    false plz

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let alertMessageWrapperElem = document.getElementById("alertMessageWrapper");
 let alertButtonWrapperElem = document.getElementById("alertButtonWrapper");
 
 
-showUI(true);
+showUI(false);
 
 window.addEventListener("keyup", (event) => {
     var activeElement = document.activeElement;
